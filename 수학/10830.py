3495e2c21f3b450310ab26cb4d1be8812241f32e
mayeonn <21900207@handong.edu>
2023-09-14 14:27:38 +0900

    행렬 제곱_빠른 거듭제곱 알고리듬

--- 수학/10830.py
+++ 수학/10830.py
@@ -1,44 +1,36 @@
-import numpy as np
+import sys
+# numpy 사용하지 않기 (시간초과)
 
-N, B = map(int, input().split())
-A = [list(map(int, input().split())) for _ in range(N)]
-
-
-# 행렬 곱셈
-def mul_matrix(matrix1, matrix2):
-    # 0으로 채운 N*N 행렬 초기 생성
-    result = [[0 for _ in range(N)] for _ in range(N)]
-
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
-                result[i][j] += matrix1[i][k] * matrix2[k][j] % 1000
+def multiplyMatrix(m1, m2):
+    length = len(m1)
+    result = [[0]*length for _ in range(length)]
+    for i in range(length):
+        for j in range(length):
+            for k in range(length):
+                result[i][j] += m1[i][k]*m2[k][j]
+                result[i][j] %= 1000    # 큰 수 연산 줄이기
     return result
 
-
-# 행렬 A를 B제곱  -  행렬 곱셈을 반복(거듭제곱을 반복)
-"""
-B가 4(짝수)인 경우, 
-    AAAA = (A^2)^2 = A^(B//2)
-B가 5(홀수)인 경우, A를 한번 더 곱함
-    AAAAA = ((A^2)^2)A = A^(B//2 + 1)
-"""
-def square(a, b):
-    if b==1:    # b가 1이 될 때까지 재귀
-        return a
+def recursivelyMultiplying(matrix, b):
+    if b==1:
+        return matrix
     else:
-        tmp = square(a, b//2)
+        # B가 짝수면  A^n 을 (A^2)^n/2 로 바꾸어  A를 곱하고, N을 2로 나눈다
+        if b%2==0:
+            matrix = recursivelyMultiplying(matrix, b//2)
+            return multiplyMatrix(matrix, matrix)
+        # B가 홀수면 A^n 을 A^(n-1) * A 로 바꾸고 result에 A를 곱한다.
+        else:
+            matrix = recursivelyMultiplying(matrix, b-1)
+            return multiplyMatrix(A, matrix)
 
-        if b%2==0:  # b가 짝수인 경우
-            return mul_matrix(tmp, tmp)
-        else:  # b가 홀수인 경우
-            return mul_matrix(mul_matrix(tmp, tmp), a)
 
+N, B = map(int, sys.stdin.readline().split())
+A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
+result = recursivelyMultiplying(A, B)
 
-result = square(A, B)
-
-for row in result:
-    for col in row:
-        print(col%1000, end=" ")
+for i in range(N):
+    for j in range(N):
+        print(result[i][j]%1000, end=' ')
     print()
