fa1bf18f87efe19f5b46afeb0cc31be236e16181
mayeonn <mayeonn9@gmail.com>
2023-09-25 14:39:30 +0900

    연속합

--- DP/1912.py
+++ DP/1912.py
@@ -6,9 +6,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
+m = list(map(int, sys.stdin.readline().split()))
 
 for i in range(1, n):
     m[i] = max(m[i], m[i]+m[i-1])
 
-print(max(arr))
+print(max(m))
