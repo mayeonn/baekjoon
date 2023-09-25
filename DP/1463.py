1a46f69aa30342fb3736b25f4ed026ae190c5c37
mayeonn <mayeonn9@gmail.com>
2023-09-25 16:27:18 +0900

    1로 만들기_DP

--- DP/1463.py
+++ DP/1463.py
@@ -1,13 +1,13 @@
 N = int(input())
-cnt = 0
-while N%3%2!=0:
-    N -= 1
-    cnt+=1
+dp = [0]*(N+1)
 
-while N!=1:
-    if N%3==0:
-        N /= 3
-    elif N%2==0:
-        N /= 2
-    cnt += 1
-print(cnt)
+# index 1부터 1
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1 # -1
+    # dp[4]이면 dp[2] 값을 사용
+    if i%2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i%3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+
+print(dp[N])
